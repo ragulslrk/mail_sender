fdf24e7f8e63090c5bd2ef774a3d0e898e316a84
ragulslrk <slrk4444@gmail.com>
2022-12-15 11:28:20 +0530

    ff

---
+++ amethyst.js
@@ -0,0 +1,30 @@
+const route =require('express').Router()
+const nodemailer=require("nodemailer")
+
+let transporter2= {
+    service: 'gmail',
+    auth: {
+    user:'amethyst3724@gmail.com'    ,
+    pass:'jryvsuugbwnwstpp',
+    },
+
+};
+const smtpTransport2 = nodemailer.createTransport(transporter2)
+
+route.post('/sendmail_amethyst',(req,res)=>{
+    const mailOptions2 = {
+                from:'amethyst3724@gmail.com',
+               to:req.body.email,
+               subject: `Confirmation Mail (amethyst Chatbot)`,
+               html: `Thank you for shopping at amethyst.we are already working hard to prepare your order.Because it is currently being processed,we'll be in touch again soon.You will receive your order within 3 - 7 days.As for you will not we charged for an delivery fee.Stay happy,healthy,glowy and don't forget to BE YOU
+               ~Team AMETHYST`
+                   }
+           smtpTransport2.sendMail(mailOptions2, function (err, info) {
+                       if (err) {
+                         console.log(err);
+                       } else {
+                           console.log('gmail sent 2');
+                           res.sendStatus(200)
+                       }})
+   })
+module.exports=route
---
+++ server.js
@@ -0,0 +1,48 @@
+const  express=require('express')
+const nodemailer=require("nodemailer")
+const fs = require("fs")
+const path = require("path")
+const app=express()
+app.use(express.urlencoded({extended:true}));   
+
+
+let transporter1= {
+    service: 'gmail',
+    auth: {
+    user:'candy.chatbot@gmail.com'    ,
+    pass:'mynhnjxhhfyatkcv',
+    },
+
+};
+
+const smtpTransport1 = nodemailer.createTransport(transporter1)
+
+
+app.post('/sendmail_candy',(req,res)=>{
+
+ const mailOptions1 = {
+             from:'candy.chatbot@gmail.com',
+            to:req.body.email,
+            subject: `Confirmation Mail (Candy Chatbot)`,
+            html: 'Your enquiry has been recorded and the college management will contact you as soon as possible. Thank you! :)'
+                }
+        smtpTransport1.sendMail(mailOptions1, function (err, info) {
+                    if (err) {
+                      console.log(err);
+                    } else {
+                        console.log('gmail sent 1');
+                        res.sendStatus(200)
+                    }})
+})
+
+//route  to  amethyst 
+const  sen_amy=require('./amethyst')
+app.use(sen_amy)
+
+
+app.get('/',(req,res)=>{
+ res.send('mail sender  is working')
+})
+app.listen(process.env.PORT ||3232,()=>{
+
+        console.log('success mailsender')})
